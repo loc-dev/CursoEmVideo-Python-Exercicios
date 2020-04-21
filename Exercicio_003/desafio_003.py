ba900ad38fd8873d3e5dbd1704413f67dd7e64dd
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-04-20 23:11:14 -0400

    Desafio 003 - Fazendo a soma entre dois números

---
+++ Exercicio_003/desafio_003.py
@@ -0,0 +1,10 @@
+# Desafio 003 - Referente aula Fase06
+# Crie um programa que leia dois números
+# e mostre a soma entre eles.
+
+n_01 = int(input('Digite o primeiro valor: '))
+n_02 = int(input('Digite o segundo valor: '))
+
+soma = n_01 + n_02
+print('')
+print('A soma entre {} e {} é igual a {}!'.format(n_01, n_02, soma))

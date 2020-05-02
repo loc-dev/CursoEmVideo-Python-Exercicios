324361531e948cdcfc7eef0584d26440d63109b5
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-05-01 21:13:18 -0400

    Desafio 005 - Antecessor e Sucessor do valor digitado - Versão 02

---
+++ Exercicio_005/desafio_005_02.py
@@ -0,0 +1,9 @@
+# Desafio 005 - Referente aula Fase07
+# Faça um programa que leia um número inteiro
+# e mostre na tela o seu sucessor e seu antecessor.
+
+# Versão 02
+
+num = int(input('Digite um número: '))
+
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(num, (num - 1), (num + 1)))

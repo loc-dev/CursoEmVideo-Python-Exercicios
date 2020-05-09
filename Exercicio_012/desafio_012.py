ec1556faa6924b485224fd060c05557e216be867
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-05-08 21:28:43 -0400

    Desafio 012 - Calculando Descontos - Versão 01

---
+++ Exercicio_012/desafio_012.py
@@ -0,0 +1,11 @@
+# Desafio 012 - Referente aula Fase07
+# Faça um algoritmo que leia o preço de um produto
+# e mostre seu novo preço, com 5% de desconto.
+
+# Versão 01
+
+price = float(input('Qual é o preço do produto? R$'))
+
+new_price = ((price * 5) / 100)
+
+print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}'.format(price, (price - new_price)))

8f368c5a745621ed38685986da2927e331a4e328
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-06-15 16:55:31 -0400

    Desafio 016 - Parte inteira - Versão 02

---
+++ Exercicio_016/desafio_016_02.py
@@ -0,0 +1,9 @@
+# Desafio 016 - Referente aula Fase08
+# Crie um programa que leia um número Real qualquer pelo teclado
+# e mostre na tela a sua porção inteira.
+
+# Versão 02
+
+valor = float(input('Digite um número: '))
+
+print('O número digitado anteriormente foi {} e a sua parte inteira é {}.'.format(valor, int(valor)))
